20778e2bcd76b1427795ff46a45abc1413fe07ab
nasserahmad8 <nalnakha2025@students.curry.edu>
2025-05-06 12:59:20 -0400

    Add files via upload

---
+++ Alnakhala_CapstoneDeckStubs.java
@@ -0,0 +1,50 @@
+public class Alnakhala_CapstoneDeckStubs {
+    public static void main(String[] args) {
+        // Will create and display deck
+    }
+    
+    public Alnakhala_CapstoneDeckStubs() {
+        
+    }
+    
+    public void createDeck() {
+        // Will create all 52 cards
+    }
+    
+    public void displayDeck() {
+        // Will display all cards in the deck
+    }
+    
+    public String rank() {
+        // Will return a rank
+    }
+    
+    public String suit() {
+        // Will return a suit
+    }
+}
+
+class Card {
+    public String rank;
+    public String suit;
+    
+    public Card(String rank, String suit) {
+        
+    }
+}
+
+class Deck {
+    private Card[] cards;
+    
+    public Deck() {
+        
+    }
+    
+    public void buildDeck() {
+        // Will build all 52 cards
+    }
+    
+    public Card[] cards() {
+        // Will return the array of cards
+    }
+}
---
+++ Alnakhala_CapstoneShuffleStubs.java
@@ -0,0 +1,48 @@
+public class Alnakhala_CapstoneShuffleStubs {
+    public static void main(String[] args) {
+        new Alnakhala_CapstoneShuffleStubs();
+    }
+
+   
+    public void DeckThatIsShuffled() { // constructor
+        // initialize the ranks and the suits
+        // suits = { "clubs", "diamonds", "hearts", "spades" };
+        // ranks = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+           //               "jack", "queen", "king" };
+                          
+        // create the cards array
+        
+        // make the cards array exactly the right size for all the cards
+        
+        // add rank to each card
+        
+        // add suit to each card
+    }
+    
+    public String[] addSuitToEachCard(String[] cards, String[] suits) {
+
+    }
+    
+    public String[] addRankToEachCard(String[] cards, String[] ranks) {
+    }
+    
+    public String[] createArrayForCards(int rankLength, int suitLength) {
+        // make the cards array exactly the right size for all the cards 
+        
+    }
+    
+    public void displayDeckOfCards(String[] cards) {
+    }
+    
+    // Adding the shuffle stub methods
+    public void shuffleDeck(String[] cards) {
+        // code to shuffle the deck
+        // Select the card at that random position from the original deck
+        // Mark that position as used (-1) to avoid selecting it again
+        // If the number is already chosen find the next available position
+    }
+    
+    public void displayShuffledDeck(String[] shuffledCards) {
+        // code to display the shuffled deck
+    }
+}
